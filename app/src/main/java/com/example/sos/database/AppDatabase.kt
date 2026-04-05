3c224f5dc142b8ceae8b77c9e15c28c468e015b5
Arda Demir <ardademir4207@gmail.com>
2026-04-05 17:10:16 +0300

    CHAT ONCESI TEMIZ

--- app/src/main/java/com/example/sos/database/AppDatabase.kt
+++ app/src/main/java/com/example/sos/database/AppDatabase.kt
@@ -5,8 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.sos.database.ContactEntity
 import com.example.sos.database.ContactDao
+import com.example.sos.database.ContactEntity
 
 @Database(entities = [DogtagEntity::class, ContactEntity::class], version = 4, exportSchema = false)
 @TypeConverters(StringListConverter::class)
